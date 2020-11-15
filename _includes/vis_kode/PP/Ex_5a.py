1199f1eb5817450c3da5c7825f43a7b516a7264b
Henrik L. Njølstad <henrik.njolstad@uia.no>
2020-11-15 21:59:19 +0100

    Lenker til eksterne sider åpnes i ny fane

--- _includes/vis_kode/PP/Ex_5a.py
+++ _includes/vis_kode/PP/Ex_5a.py
@@ -1,7 +1,7 @@
-def finne_felles_elementer(liste1, liste2):
-    svarliste = []
+def felles_elementer(liste1, liste2):
+    felleselementer = []
     for x in liste1:
         for y in liste2:
-            if x == y and x not in svarliste:
-                svarliste.append(x)
-    return svarliste
+            if x == y and x not in felleselementer:
+                felleselementer.append(x)
+    return felleselementer
