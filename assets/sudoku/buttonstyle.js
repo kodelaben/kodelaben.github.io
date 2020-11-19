a996b2cb1e2ec2d27493f548fc658cbcfa27d7f7
Henrik Løvik Njølstad <henrln14@uia.no>
2020-11-19 14:53:40 +0100

    fiksa knapp

--- assets/sudoku/buttonstyle.js
+++ assets/sudoku/buttonstyle.js
@@ -1,20 +1,31 @@
+let pauseknapp = 1;
 var button = document.getElementById("example-three");
 button.addEventListener('click', function () {
   if (button.getAttribute("data-text-swap") == button.innerHTML) {
     button.innerHTML = button.getAttribute("data-text-original");
+    pauseknapp = 1;
   } else {
     button.setAttribute("data-text-original", button.innerHTML);
     button.innerHTML = button.getAttribute("data-text-swap");
+    pauseknapp = 0;
   }
 }, false);
 
+
 var button2 = document.getElementById("example-three231");
 button2.addEventListener('click', function () {
-  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+  if (pauseknapp == 0) {
     button.innerHTML = button.getAttribute("data-text-original");
-  } else {
-    button.setAttribute("data-text-original", button.innerHTML);
-    button.innerHTML = button.getAttribute("data-text-swap");
+    pauseknapp = 1;
   }
 }, false);
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("sudokuspan");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+  fps = this.value;
+}
 
