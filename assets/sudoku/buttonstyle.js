00defa19d079c1b6a4998996fbe2e65e0cec0d8e
Henrik Løvik Njølstad <henrln14@uia.no>
2020-11-19 13:08:38 +0100

    Pause/un-pause knapp

---
+++ assets/sudoku/buttonstyle.js
@@ -0,0 +1,9 @@
+var button = document.getElementById("example-three");
+button.addEventListener('click', function () {
+  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+  }
+}, false);
